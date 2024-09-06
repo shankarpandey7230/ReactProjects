8f4254a213372476087134f7d6498faf2994780a
shankarpandey7230 <shankarpandey7230@gmail.com>
2024-09-06 23:31:37 +1000

    setting up tabs project

---
+++ tabs/src/App.jsx
@@ -0,0 +1,10 @@
+const url = 'https://course-api.com/react-tabs-project';
+
+import { useState } from 'react';
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <>tabs</>;
+}
+export default App;
