0b5168ac3fd9c80c73b7c8b34bc6da9cd5d335e1
shankarpandey7230 <shankarpandey7230@gmail.com>
2024-09-08 03:51:01 +1000

    finishing up the slider project

--- slider/src/App.jsx
+++ slider/src/App.jsx
@@ -1,7 +1,14 @@
 import React from 'react';
+import Carousel from './Carousel';
+import SlickCarousel from './SlickCarousel';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <main>
+      {/* <Carousel /> */}
+      <SlickCarousel />
+    </main>
+  );
 };
 
 export default App;
